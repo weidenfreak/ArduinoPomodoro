7465892c9bd6493d64383befa123c9cbf2cc253c
weidenfreak <meike.wiemann@gmx.de>
2014-12-10 17:09:42 +0100

    initial setup rails and angular

---
+++ pomodoro/app/assets/javascripts/app.coffee
@@ -0,0 +1,2 @@
+pomodoro = angular.module('pomodoro',[
+])
---
+++ pomodoro/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pomodoro::Application.config.secret_key_base = '3fa93829778e9efe334bb2731116dba41930c04e18db45159332d1fd1c40aeb5c720e2cd72e45474a577d1f24409a82271fd8202b685fef913fde5988d65194a'
